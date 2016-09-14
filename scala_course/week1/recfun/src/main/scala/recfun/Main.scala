bacbfcd377b69247b0dabfefb50addc9cf672942
Artemii Bezguzikov <artemii.bezguzikov@gmail.com>
2016-09-14 21:08:05 +0300

    Add pascal task

--- scala_course/week1/recfun/src/main/scala/recfun/Main.scala
+++ scala_course/week1/recfun/src/main/scala/recfun/Main.scala
@@ -13,7 +13,16 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if (c > r) throw new java.util.NoSuchElementException()
+      if (c == r) 1
+      else
+        (c, r) match {
+          case (0, _) => 1
+          case (1, n) => n
+          case (c, r) => pascal(c - 1, r - 1) + pascal(c, r - 1)
+        }
+    }
   
   /**
    * Exercise 2
